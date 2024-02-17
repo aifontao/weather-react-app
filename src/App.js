608d17320758207de5d6553acd2ec7123285e1df
aifontao <anaisabelfontao@gmail.com>
2024-02-17 12:20:09 +0000

    Added the footer with links to Github repository and Netlify

--- src/App.js
+++ src/App.js
@@ -4,6 +4,32 @@ function App() {
   return (
     <div className="App">
       <h1>Weather app</h1>
+      <footer>
+        This react app was coded by{" "}
+        <a
+          href="https://github.com/aifontao?tab=repositories"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Ana Fontão
+        </a>
+        , it's opensourced on{" "}
+        <a
+          href="https://github.com/aifontao/weather-react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Github
+        </a>{" "}
+        and hosted on{" "}
+        <a
+          href="https://weather-react-app-aif.netlify.app/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Netlify
+        </a>
+      </footer>
     </div>
   );
 }
