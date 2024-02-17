1d3f48f6f8ba273b9704baa801e37463aa2a6410
aifontao <anaisabelfontao@gmail.com>
2024-02-17 12:42:54 +0000

    Defined app's structure

--- src/Weather.js
+++ src/Weather.js
@@ -1,14 +1,24 @@
 import React from "react";
 import axios from "axios";
+import "./Weather.css";
 
 export default function Weather() {
   return (
-    <div className="Weather">
+    <div className="Weather Container">
       <form>
         <input type="search" placeholder="Enter a city..."></input>
         <input type="submit" value="Search"></input>
       </form>
-      <p>The currente weather is 19C</p>
+      <div className="CurrentWeatherData">
+        <h1>Lisbon</h1>
+        <ul>
+          <li>Tempertature:</li>
+          <li>Description:</li>
+          <li>Humidity:</li>
+          <li>Wind:</li>
+          <li>Icon</li>
+        </ul>
+      </div>
     </div>
   );
 }
