6fbfa19ad1cbc43bd73bb0565fd54217691ec0e2
aifontao <anaisabelfontao@gmail.com>
2024-02-17 12:29:26 +0000

    Added Search form to the Weather component

--- src/Weather.js
+++ src/Weather.js
@@ -2,5 +2,13 @@ import React from "react";
 import axios from "axios";
 
 export default function Weather() {
-  return <div className="Weather">The currente weather is 19C</div>;
+  return (
+    <div className="Weather">
+      <form>
+        <input type="search" placeholder="Enter a city..."></input>
+        <input type="submit" value="Search"></input>
+      </form>
+      <p>The currente weather is 19C</p>
+    </div>
+  );
 }
